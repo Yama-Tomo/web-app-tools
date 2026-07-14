7e8e619b5a7d78d92d465056c06f7869cf5ee788
Yama-Tomo <yam4.tomo@gmail.com>
2026-07-14 14:49:55 +0900

    Bump @chakra-ui/panda-preset to 3.36.0

--- packages/panda-preset/src/recipes/index.ts
+++ packages/panda-preset/src/recipes/index.ts
@@ -1,14 +1,12 @@
 import { recipes as chakraPresetRecipes } from '#chakraPreset/recipes'
-import { omit } from '#utils'
 import { button } from './button'
 import { iconButton } from './iconButton'
 import { input } from './input'
 import { textarea } from './textarea'
 
 export const recipes = {
-  ...omit(chakraPresetRecipes, ['container']),
+  ...chakraPresetRecipes,
   button,
-  containerRecipe: chakraPresetRecipes.container, // NOTE: `container` is already exported, so use a non-conflicting name
   iconButton,
   input,
   textarea,
